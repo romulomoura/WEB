07d9f403ca5bb38aa5771e7dbaf094903787817b
Franco Marques <francopilloto@gmail.com>
2018-05-22 17:48:05 -0400

    Adding setup and home page

--- Focus App/focus.js
+++ Focus App/focus.js
@@ -1,5 +1,63 @@
 
-//This function add 0 in front of the number that are < 10
+// called when the page is loaded
+$().ready(function() 
+{
+	// check for browser support
+	if (typeof(Storage) == undefined) {		
+		showErrorPage();
+	} 
+	// check for existing user
+	else if (localStorage.getItem('username')) {
+		showHomePage();
+	}
+	// create 'new' account
+	else {
+		showSetupPage();
+	}
+});
+
+function showErrorPage()
+{
+	$('.panel').hide(); // hide all panels
+	$('.error').show(); // show the error panel
+}
+
+function showSetupPage()
+{	
+	// press ENTER on setup screen to get the user name
+	$('.setup input').off('keyup.setup').on('keyup.setup', function(e) 
+	{
+		if (e.keyCode == 13) // 'enter' keyCode
+		{
+			// check if the username is valid
+			var name = $(this).val();
+			
+			if (name && name.length > 0)
+			{
+				// save it to the broser storage
+				localStorage.setItem('username', name);
+				
+				// go to home screen
+				$('.setup').fadeOut(1000, function() {
+					showHomePage();
+				});							
+			}
+		}
+	});
+	
+	$('.panel').hide(); // hide all panels
+	
+	// show setup panel gently
+	$('.setup').fadeIn(3000).find('input').focus();
+}
+
+function showHomePage()
+{
+	$('.home').fadeIn(3000).find('input').focus();
+	showTime();
+}
+
+// this function add 0 in front of the number that are < 10
 function formatTime(i) {
   if (i < 10) {
     i = "0" + i;
@@ -7,39 +65,23 @@ function formatTime(i) {
   return i;
 }
 
-//this function show the time each 0,5s
-function showTime() {
-  //Get current date
+// update current time
+function showTime() 
+{
+  // get current date
   var today = new Date();
-  var h = today.getHours();
-  var m = today.getMinutes();
-  var s = today.getSeconds();
   
-  // add a zero in front of numbers<10
-  m = formatTime(m);
-  s = formatTime(s);
-  document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
-  showGreeting(h);  
-  //update the time information on the screen
+  var hour = today.getHours();
+  var minutes = today.getMinutes();   
+  
+  $('.home h1').html(formatTime(hour) + ":" + formatTime(minutes));
+  
+  var period = hour < 12 ? 'morning' : (hour < 18 ? 'afternoon' : 'evening');
+  
+   $('.home h2').html('Good ' + period + ', ' + localStorage.getItem('username') + '.');
+    
+  // update the time information on the screen
   t = setTimeout(function() {
     showTime()
-  }, 500);
+  }, 1000);
 }
-
-//This function show a especific greeting according to the time
-function showGreeting(h) {
-
-	if ((h>00) && (h<12))
-	{
-		document.getElementById('greeting').innerHTML = "Good morning!";
-	}
-	else if ((h>12) && (h<18))
-	{
-		document.getElementById('greeting').innerHTML = "Good afternoon!";
-	}
-	else if ((h>18) && (h<=00)){
-		document.getElementById('greeting').innerHTML = "Good evening!";
-	}
-}
-
-showTime();
