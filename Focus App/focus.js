386b458b94764b3e67053239fcd1d3d5070f8e2b
Franco Marques <francopilloto@gmail.com>
2018-05-23 14:26:49 -0400

    Adding background image

--- Focus App/focus.js
+++ Focus App/focus.js
@@ -5,23 +5,38 @@ $().ready(function()
 	// check for browser support
 	if (typeof(Storage) == undefined) {		
 		showErrorPage();
-	} 
-	// check for existing user
-	else if (localStorage.getItem('username')) {
-		showHomePage();
 	}
-	// create 'new' account
-	else {
-		showSetupPage();
+	else
+	{
+		// check for existing user
+		if (localStorage.getItem('username')) {
+			showHomePage();
+		}
+		// or create 'new' account
+		else {
+			showSetupPage();
+		}
+		
+		// set timer to auto update background image
+		updateBackgroundImage();
+		
+		setInterval(function(){
+			//updateBackgroundImage();
+		}, 1*60*1000);
 	}
 });
 
+// show an error message on the screen if the browser
+// does not support local storage
 function showErrorPage()
 {
 	$('.panel').hide(); // hide all panels
 	$('.error').show(); // show the error panel
 }
 
+// the setup page will get the user name and save it in the 
+// browser local storage. 
+// this page will only be displayed if no username has been defined beffore.
 function showSetupPage()
 {	
 	// press ENTER on setup screen to get the user name
@@ -51,6 +66,7 @@ function showSetupPage()
 	$('.setup').fadeIn(3000).find('input').focus();
 }
 
+// the main page. if a username has set, this will be the first page.
 function showHomePage()
 {
 	$('.home').fadeIn(3000).find('input').focus();
@@ -58,14 +74,16 @@ function showHomePage()
 }
 
 // this function add 0 in front of the number that are < 10
-function formatTime(i) {
+function formatTime(i) 
+{
   if (i < 10) {
     i = "0" + i;
   }
+  
   return i;
 }
 
-// update current time
+// update current time on screen
 function showTime() 
 {
   // get current date
@@ -84,4 +102,23 @@ function showTime()
   t = setTimeout(function() {
     showTime()
   }, 1000);
+}
+
+// retrieves the background image from 'unsplash' website
+function updateBackgroundImage()
+{
+	var unsplashUrl = "https://api.unsplash.com/photos/random?featured";
+	var unsplashClientId = "a24e1fa3b77c93935f0552ebf6354a3540e9aa356caf80a1fcb55125fdb16110";
+	
+    $.getJSON(unsplashUrl + "&client_id=" + unsplashClientId, function(data)
+	{        
+        // change data.urls.full to data.urls.regular for faster loading but 
+		// image quality will be slightly worse
+        var photoUrl = data.urls.regular;
+        
+        $("body").css("background-image", "url(" + photoUrl + ")");       
+    })
+	.fail(function(){
+		$("body").css("background-image", "url(static.jpg)");
+	});
 }
